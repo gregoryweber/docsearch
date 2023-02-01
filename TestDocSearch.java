72462e8c09be0f4c227363e29689cb1e64fba523
Gregory Weber <gregory1@weberdao.org>
2023-02-01 12:41:41 -0800

    added 1 and 0 tests

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -15,7 +15,22 @@ public class TestDocSearch {
 	public void testSearch() throws URISyntaxException, IOException {
     Handler h = new Handler("./written_2/");
     URI rootPath = new URI("http://localhost/search?q=sushi");
-    String expect = "Found 3 paths:\n./written_2/travel_guides/berlitz1/WhatToJapan.txt\n./written_2/travel_guides/berlitz1/WhereToJapan.txt\n./written_2/travel_guides/berlitz2/California-WhereToGo.txt";
+    String expect = "Found 3 paths:\n.\\written_2\\travel_guides\\berlitz1\\WhatToJapan.txt\n.\\written_2\\travel_guides\\berlitz1\\WhereToJapan.txt\n.\\written_2\\travel_guides\\berlitz2\\California-WhereToGo.txt";
+    assertEquals(expect, h.handleRequest(rootPath));
+	}
+    @Test 
+	public void test1Search() throws URISyntaxException, IOException {
+    Handler h = new Handler("./written_2/");
+    URI rootPath = new URI("http://localhost/search?q=portugal");
+    // String expect = "Found 1 paths:\n./written_2/travel_guides/berlitz2/Portugal-WhatToDo.txt";
+    String expect = "Found 1 paths:\n.\\written_2\\travel_guides\\berlitz2\\Portugal-WhatToDo.txt";
+    assertEquals(expect, h.handleRequest(rootPath));
+	}
+    @Test 
+	public void test0Search() throws URISyntaxException, IOException {
+    Handler h = new Handler("./written_2/");
+    URI rootPath = new URI("http://localhost/search?q=helloworld");
+    String expect = "Found 0 paths:\n";
     assertEquals(expect, h.handleRequest(rootPath));
 	}
 }
